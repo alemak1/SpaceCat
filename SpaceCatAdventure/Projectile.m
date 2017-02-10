e82c85b6e175e071303260efe59f94b13ea7ffc8
yongrenwang <czie.alexmak@gmail.com>
2017-02-10 15:53:58 +0800

    Created a Util class whose header file will host app constants; deleted constants.h file

--- SpaceCatAdventure/Projectile.m
+++ SpaceCatAdventure/Projectile.m
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "Projectile.h"
-#import "Constants.h"
+#import "Util"
 
 @implementation Projectile
 
--- SpaceCatAdventure/Util.h
+++ SpaceCatAdventure/Util.h
@@ -1,14 +1,20 @@
 //
-//  Constants.h
+//  Util.h
 //  SpaceCatAdventure
 //
 //  Created by Aleksander Makedonski on 2/10/17.
 //  Copyright © 2017 Changzhou Panda. All rights reserved.
 //
 
-#ifndef Constants_h
-#define Constants_h
+#ifndef Util_h
+#define Util_h
+
+#import <Foundation/Foundation.h>
 
 static const int PROJECTILE_SPEED = 500;
 
-#endif /* Constants_h */
+@interface Util : NSObject
+
+@end
+
+#endif /* Util_h */
